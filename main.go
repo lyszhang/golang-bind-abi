e41bf0d17845617a32a8cd57ab9722681447a03a
lyszhang <lyszhang@126.com>
2022-11-22 14:21:44 +0800

    feat: Initial Commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+const key = `json object from keystore`
+
+func main() {
+	// Create an IPC based RPC connection to a remote node and instantiate a contract binding
+	conn, err := ethclient.Dial("/home/go-ethereum/goerli/geth.ipc")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	store, err := NewStorage(common.HexToAddress("0x21e6fc92f93c8a1bb41e2be64b4e1f88a54d3576"), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate a Storage contract: %v", err)
+	}
+	// Create an authorized transactor and call the store function
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), "strong_password", big.NewInt(1))
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+	// Call the store() function
+	tx, err := store.Store(auth, big.NewInt(420))
+	if err != nil {
+		log.Fatalf("Failed to update value: %v", err)
+	}
+	fmt.Printf("Update pending: 0x%x\n", tx.Hash())
+
+	value, err := store.Retrieve(nil)
+	if err != nil {
+		log.Fatalf("Failed to retrieve value: %v", err)
+	}
+	fmt.Println("Value: ", value)
+}
